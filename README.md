e3bdb1fdd42de69b00434e9d9ad651955a703596
Shreyas Dorle <shreyasdorle.it@gmail.com>
2024-02-10 17:58:41 +0530

    did structuring for git

---
+++ README.md
@@ -0,0 +1,102 @@
+<a name="readme-top"></a>
+
+  
+  
+
+# ExpenseWise
+
+  
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+ExpenseWise is a simple personal finance tracking, to add transactions and track your monthly income and expenses in a simple and elegant UI. 
+  
+### Built With
+
+- NextJS
+- TailwindCSS
+- ApexCharts
+- NodeJS
+- Express
+  
+  
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+* npm
+
+  ```sh
+
+  npm install npm@latest -g
+
+  ```
+
+### Installation
+
+
+1. Clone the repo
+
+   ```sh
+
+   git clone https://github.com/your_username_/Project-Name.git
+
+   ```
+
+2. Go into server folder and install packages
+
+    ```sh
+   cd server
+   ```
+
+   ```sh
+   npm install
+   ```
+
+3. Open another terminal and do the same for client
+
+      ```sh
+   cd client
+   ```
+
+   ```sh
+   npm install
+   ```
+
+<!-- USAGE EXAMPLES -->
+
+## Usage
+
+1. Run the Express server from server terminal
+
+   ```sh
+
+   npm start
+
+   ```
+
+2. Run the NextJS app from client terminal
+
+   ```sh
+
+   npm run dev
+
+   ```
+
+3. Add transactions and check stats
+
+
+_Note: This app uses the db.json file in the server directory as a placeholder for a database, sample data is present for convenience._
+
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fsPromises = require('fs/promises');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+const dataFilePath = path.join(process.cwd(), 'db.json');
+
+app.post('/createTransaction', async(req, res) => {
+    const jsonData = await fsPromises.readFile(dataFilePath);
+    const objectData = JSON.parse(jsonData);
+    objectData.transactions.push(req.body)
+    await fsPromises.writeFile(dataFilePath, JSON.stringify(objectData));
+    res.json({"msg":"Transaction added"});
+});
+
+app.get('/fetchTransactions', async(req, res) => {
+    const jsonData = await fsPromises.readFile(dataFilePath);
+    const objectData = JSON.parse(jsonData);
+    res.json(objectData.transactions.reverse());
+});
+
+app.get('/fetchOverview', async(req, res) => {
+    const jsonData = await fsPromises.readFile(dataFilePath);
+    const objectData = JSON.parse(jsonData);
+    var monthlyIncome = 0
+    var monthlyExpenses = 0
+    var dailyIncome = []
+    var dailyExpenses = []
+    var expensesByCategory = {}
+    const currentMonth = new Date().getMonth();
+
+    objectData.transactions.forEach(transaction => {
+        const getDate = new Date(transaction.date);
+        if(getDate.getMonth()==currentMonth){
+            if(transaction.type=="sent"){
+                monthlyExpenses += parseInt(transaction.amount)
+                dailyExpenses.push({x:getDate,y:transaction.amount})
+                if(transaction.category in expensesByCategory){
+                    expensesByCategory[transaction.category] += parseInt(transaction.amount)
+                }else{
+                    expensesByCategory[transaction.category] = parseInt(transaction.amount)
+                }
+            }else if(transaction.type=="received"){
+                monthlyIncome += parseInt(transaction.amount)
+                dailyIncome.push({x:getDate,y:transaction.amount})
+            }
+        }
+        
+    });
+
+    res.json({"monthlyIncome":monthlyIncome,"monthlyExpenses":monthlyExpenses,"monthlyProfit":monthlyIncome-monthlyExpenses,"dailyIncome":dailyIncome,"dailyExpenses":dailyExpenses,"expensesByCategory":expensesByCategory,"transactions":objectData.transactions.reverse().slice(0,5)});
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
